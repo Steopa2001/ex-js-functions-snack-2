11f005ffb8ea945459d3e6db98189b764e7a37ee
Steopa2001 <moraru495@gmail.com>
2025-10-30 14:29:28 +0100

    [feat] complete Snack 6 - function that creates an automatic counter using setInterval

--- script.js
+++ script.js
@@ -79,5 +79,20 @@
 // }
 // stampaOgniSecondo("Ciao ogni secondo!");
 
+//-----------------------------------------------------------------------------------------------------------------------
 
+// SNACK 6
+// Crea un contatore automatico con setInterval
 
+// Definisci una funzione creaContatoreAutomatico che accetta un intervallo di tempo e restituisce una funzione che avvia un setInterval, incrementando un contatore e stampandolo.
+// function creaContatoreAutomatico(intervallo) {
+//   let contatore = 0;
+//   return function () {
+//     setInterval(() => {
+//       contatore++;
+//       console.log(contatore);
+//     }, intervallo);
+//   };
+// }
+// const startCounter = creaContatoreAutomatico(1000);
+// startCounter();

1855b67200626ba3c3ad32c3e452e796607f712d
Steopa2001 <moraru495@gmail.com>
2025-10-30 12:30:14 +0100

    [feat] complete Snack 1 - function that sums two numbers

--- script.js
+++ script.js
@@ -5,6 +5,19 @@
 // Poi, definisci la stessa funzione somma ma come funzione anonima assegnata a una variabile
 // Quindi, riscrivi la funzione somma con la sintassi delle arrow functions.
 
-function somma (a, b) {
-    return a + b
-}
+//funzione dichiarativa
+// function somma (a, b) {
+//     return a + b;
+// }
+// console.log(somma(10, 5))
+
+//funzione anonima
+// const somma = function(a, b) {
+//     return a + b;
+// }
+// console.log(somma(20, 20))
+
+//sintassi arrow function 
+// const somma = (a, b) => a + b;
+
+// console.log(somma(10, 7))

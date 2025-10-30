7a49c2b37ff98b1a4821eb0c863483f5dd3a62ce
Steopa2001 <moraru495@gmail.com>
2025-10-30 14:18:58 +0100

    [feat] complete Snack 5 - function that prints a message every second using setInterval

--- script.js
+++ script.js
@@ -58,3 +58,26 @@
 // }
 // const timer = creaTimer(2000);
 // timer();
+
+//-----------------------------------------------------------------------------------------------------------------------
+
+// SNACK 5
+// Crea una funzione stampaOgniSecondo con setInterval.
+
+// Definisci una funzione che accetta un messaggio e lo stampa ogni secondo.
+// Nota: Questa funzione creerà un loop infinito. Interrompilo manualmente o usa clearInterval() in un altro script.
+// function stampaOgniSecondo(messaggio) {
+//   const intervalId = setInterval(() => {
+//     console.log(messaggio);
+//   }, 1000);
+
+//   // Interrompe dopo 5 secondi
+//   setTimeout(() => {
+//     clearInterval(intervalId);
+//     console.log("Intervallo fermato!");
+//   }, 5000);
+// }
+// stampaOgniSecondo("Ciao ogni secondo!");
+
+
+

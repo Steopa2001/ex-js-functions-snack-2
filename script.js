865398a3ee8a7fca1073ea877b4c3f4e1c2c82fd
Steopa2001 <moraru495@gmail.com>
2025-10-31 11:26:53 +0100

    [feat] complete Snack 7 - function that runs a timer and stops it after a set time

--- script.js
+++ script.js
@@ -96,3 +96,21 @@
 // }
 // const startCounter = creaContatoreAutomatico(1000);
 // startCounter();
+
+//-----------------------------------------------------------------------------------------------------------------------
+
+// SNACK 7
+// Crea una funzione che ferma un timer dopo un certo tempo
+
+// Scrivi una funzione eseguiEferma che accetta un messaggio, un tempo di avvio e un tempo di stop. Il messaggio deve essere stampato a intervalli regolari, ma si deve fermare dopo il tempo di stop.
+// function eseguiEferma(messaggio, avvio, stop) {
+//   const intervalId = setInterval(() => {
+//     console.log(messaggio);
+//   }, avvio);
+
+//   setTimeout(() => {
+//     clearInterval(intervalId);
+//     console.log("Timer fermato!");
+//   }, stop);
+// }
+// eseguiEferma("Ciao ogni secondo!", 1000, 5000);

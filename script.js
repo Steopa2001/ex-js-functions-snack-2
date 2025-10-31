9b0257d65e4704c7a29455b088c307dfe185ea34
Steopa2001 <moraru495@gmail.com>
2025-10-31 12:02:09 +0100

    [feat] complete Snack 9 - sequential operation executor using for loop and setTimeout

--- script.js
+++ script.js
@@ -134,3 +134,26 @@
 //   }, 1000);
 // }
 // contoAllaRovescia(5);
+
+
+//-----------------------------------------------------------------------------------------------------------------------
+
+// SNACK 9 
+// Creare una funzione che esegue una sequenza di operazioni con ritardi
+
+// Scrivi una funzione sequenzaOperazioni che accetta un array di operazioni (funzioni) e un tempo di intervallo.
+// Ogni operazione deve essere eseguita in sequenza con un ritardo uguale al tempo di intervallo.
+// function sequenzaOperazioni(operazioni, intervallo) {
+//   for (let i = 0; i < operazioni.length; i++) {
+//     setTimeout(() => {
+//       operazioni[i]();
+//     }, intervallo * i);
+//   }
+// }
+// sequenzaOperazioni([
+//   () => console.log("Operazione 1"),
+//   () => console.log("Operazione 2"),
+//   () => console.log("Operazione 3")
+// ], 2000);
+
+
